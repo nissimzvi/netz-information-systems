b4c4cda5b20a847358e9a70b2b1794f6cb54c3da
nissimzvi <nzvi576@gmail.com>
2026-08-13 16:50:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 document.querySelectorAll('[data-wa]').forEach(el=>{
-  const msg=encodeURIComponent('שלום, הגעתי דרך אתר נץ מערכות מידע. נשמח לדבר על תהליך שצריך לייעל.');
+  const msg=encodeURIComponent('שלום, הגעתי דרך אתר נץ מערכות מידע. נשמח לדבר על תהליך או פתרון שצריך לקדם.');
   el.href='https://wa.me/972508673333?text='+msg;
 });
